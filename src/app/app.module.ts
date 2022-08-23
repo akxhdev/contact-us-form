b1bed079d002700fc000524ed3aaf3f1f765c7a7
akxhdev <akxhdev@gmail.com>
2022-08-23 13:26:32 +0530

    optimize for mobile devices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InterestButtonComponent } from './components/interest-button/interest-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterestButtonComponent
   ],
   imports: [
     BrowserModule
